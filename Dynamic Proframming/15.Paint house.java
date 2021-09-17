17b905277a0f56eb602f37308b862fcb540bc2f6
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-09-17 15:54:34 +0530

    Update 15.Paint house.java

--- Dynamic Proframming/15.Paint house.java
+++ Dynamic Proframming/15.Paint house.java
@@ -26,7 +26,7 @@ public class Main {
           dp[i][1]=arr[i][1]+Math.min(dp[i-1][0],dp[i-1][2]);
           dp[i][2]=arr[i][2]+Math.min(dp[i-1][1],dp[i-1][0]);
       }
-      long ans=Math.min(dp[n-1][0],Math.min(dp[n-1][1],dp[n-1][2]);
+      long ans=Math.min(dp[n-1][0],Math.min(dp[n-1][1],dp[n-1][2]));
       
       System.out.println(ans);
         
