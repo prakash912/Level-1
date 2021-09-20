7814f38ed3867f8546ddcd4b721471d0f83775e3
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-09-20 18:44:40 +0530

    Create 5.Add first in LL.java

---
+++ Linked List/5.Add first in LL.java
@@ -0,0 +1,12 @@
+   public void addFirst(int val) {
+      // write your code here
+      Node temp=new Node();
+      temp.data =val; 
+      temp.next=head;
+      head=temp;
+      if(size==0){
+          tail=temp;
+      }
+      size++;
+    }
+  }
