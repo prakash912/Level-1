eedb2849acd94b881aec91c93e4ef3c6a59b349d
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-09-20 10:45:16 +0530

    Create 16.Dynamic  queue.java

---
+++ Stack/16.Dynamic  queue.java
@@ -0,0 +1,21 @@
+    void add(int val) {
+      // write ur code here
+      if(size == data.length){
+        int[] ndata=new int[2*data.length];
+        for(int i=0; i<size;i++){
+            int idx=(front+i)%data.length;
+            ndata[i]=data[idx];
+        }
+        data=ndata;
+        front=0;
+        
+          int idx = (front + size) % data.length;
+        data[idx] = val;
+        size++;
+        
+      } else {
+        int idx = (front + size) % data.length;
+        data[idx] = val;
+        size++;
+      }
+    }
