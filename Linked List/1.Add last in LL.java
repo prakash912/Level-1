74c0a0f5ec6795f9675cb2ccfe53f1a9d878423f
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-09-20 17:52:32 +0530

    Create 1.Add last in LL.java

---
+++ Linked List/1.Add last in LL.java
@@ -0,0 +1,25 @@
+public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      // Write your code here
+      Node temp=new Node();
+      temp.data=val;
+      temp.next=null;
+      if(size==0){
+          head=tail=temp;
+      }else{
+          tail.next=temp;
+          tail=temp;
+          
+      }
+      size++;
+    }
+  }
