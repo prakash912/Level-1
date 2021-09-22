826ac4f1c1cb9c1ec9cff3d39eaec69316d89d64
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-09-22 07:55:41 +0530

    Create 12.Node to root path.java

---
+++ Genric Tree/12.Node to root path.java
@@ -0,0 +1,18 @@
+ public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    // write your code here
+      if (node.data == data) {
+      ArrayList<Integer> path = new ArrayList<>();
+      path.add(node.data);
+      return path;
+    }
+
+    for (Node child : node.children) {
+      ArrayList<Integer> ptc = nodeToRootPath(child, data);
+      if (ptc.size() > 0) {
+        ptc.add(node.data);
+        return ptc;
+      }
+    }
+
+    return new ArrayList<>();
+ }
