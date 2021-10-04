e00415ae6a5451565ae4fdc3243d0b89462fc39a
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-10-04 19:28:50 +0530

    Create 1.max,min,size.java

---
+++ Binary Serch Tree/1.max,min,size.java
@@ -0,0 +1,53 @@
+ public static int size(Node node) {
+    // write your code here
+    if(node==null){
+        return 0;
+    }
+    int ls=size(node.left);
+    int rs=size(node.right);
+    int ts=ls+rs+1;
+    return ts;
+  }
+
+  public static int sum(Node node) {
+    // write your code here
+    if(node==null){
+        return 0;
+    }
+    int ls=sum(node.left);
+    int rs=sum(node.right);
+    int ts=ls+rs+node.data;
+    return ts;
+  }
+
+  public static int max(Node node) {
+    // write your code here
+    if(node.right==null){
+        return node.data;
+    }else{
+        return max(node.right);
+    }
+  }
+
+  public static int min(Node node) {
+    // write your code here
+    if(node.left==null){
+        return node.data;
+    }else{
+        return min(node.left);
+    }
+  }
+
+  public static boolean find(Node node, int data){
+    // write your code here
+    if(node==null){
+        return false;
+    }
+    if(data>node.data){
+        return find(node.right,data);
+    }else if(data<node.data){
+        return find(node.left,data);
+    }else{
+        return true;
+    }
+  }  
